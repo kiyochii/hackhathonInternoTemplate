550eb3c05db09b5fcfe9db365f4930bf471848d3
henriquemantovan <henrique.mantovan@usp.br>
2024-08-21 00:18:02 -0300

    primeira page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,59 +1,41 @@
-import { useAccount, useConnect, useDisconnect, useSwitchChain } from "wagmi";
-import { hardhat, sepolia } from "wagmi/chains";
-import Contract from "./Contract";
+//import { useAccount, useConnect, useDisconnect, useSwitchChain } from "wagmi";
+//import { hardhat, sepolia } from "wagmi/chains";
+//import Contract from "./Contract";
+import agendaIcon from './images/agenda.jpg'; 
+import pedidoIcon from './images/pedido.png'; // Ajuste o caminho conforme necessário
 
-const CHAIN_ID = import.meta.env.DEV ? hardhat.id : sepolia.id;
+
+
+//const CHAIN_ID = import.meta.env.DEV ? hardhat.id : sepolia.id;
 
 function App() {
-  const account = useAccount();
-  const { connectors, connect, status, error } = useConnect();
-  const { switchChain } = useSwitchChain();
-  const { disconnect } = useDisconnect();
+  //const account = useAccount();''
+  //const { connectors, connect, status, error } = useConnect();
+  //const { switchChain } = useSwitchChain();
+  //const { disconnect } = useDisconnect();
 
-  const isCorrectChain = CHAIN_ID === account.chainId;
+  //const isCorrectChain = CHAIN_ID === account.chainId;
 
   return (
-    <>
-      <div>
-        <h2>Account</h2>
+    <main style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '90vh', textAlign: 'center' }}>
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        <h1 style={{ fontSize: '100px', marginBottom: '20px' }}>
+          To Doing
+        </h1>
+        
+        <button style={{ display: 'flex', alignItems: 'center', padding: '20px', fontSize: '20px', margin: '15px 0' }}>
+          <img src={agendaIcon} style={{ width: '50px', height: '50px', marginRight: '10px' }} />
+          Tarefas disponíveis
+        </button>
 
-        <div>
-          status: {account.status}
-          <br />
-          addresses: {JSON.stringify(account.addresses)}
-          <br />
-          chainId: {account.chainId}
-        </div>
+        <button style={{ display: 'flex', alignItems: 'center', padding: '20px', fontSize: '20px', margin: '15px 0' }}>
+          <img src={pedidoIcon} style={{ width: '50px', height: '50px', marginRight: '10px' }} />
+          Criar nova tarefa
+        </button>
 
-        {account.status === "connected" && (
-          <button type="button" onClick={() => disconnect()}>
-            Disconnect
-          </button>
-        )}
       </div>
-
-      <div>
-        <h2>Connect</h2>
-        {connectors.map((connector) => (
-          <button key={connector.uid} onClick={() => connect({ connector })} type="button">
-            {connector.name}
-          </button>
-        ))}
-        <div>{status}</div>
-        <div>{error?.message}</div>
-      </div>
-      <div>
-        <h2>Contract</h2>
-        {isCorrectChain ? (
-          <Contract />
-        ) : (
-          <button type="button" onClick={() => switchChain({ chainId: CHAIN_ID })}>
-            Switch chain
-          </button>
-        )}
-      </div>
-    </>
+    </main>
   );
 }
-
-export default App;
+  
+  export default App;
