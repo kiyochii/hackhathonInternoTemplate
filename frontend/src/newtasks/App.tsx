f6c2b582d80d3088450e1cd06bc57fefc22de513
henriquemantovan <henrique.mantovan@usp.br>
2024-08-21 21:55:54 -0300

    primeira pag quase pronta

--- frontend/src/newtasks/App.tsx
+++ frontend/src/newtasks/App.tsx
@@ -2,9 +2,6 @@
 //import { hardhat, sepolia } from "wagmi/chains";
 //import Contract from "./Contract";
 
-import agendaIcon from './images/agenda.jpg'; 
-import pedidoIcon from './images/pedido.png'; // Ajuste o caminho conforme necessário
-
 //const CHAIN_ID = import.meta.env.DEV ? hardhat.id : sepolia.id;
 
 function App() {
@@ -21,15 +18,6 @@ function App() {
         <h1 style={{ fontSize: '100px', marginBottom: '20px' }}>
           To Doing
         </h1>
-          <button className='botao' style={{ display: 'flex', alignItems: 'center', padding: '20px', fontSize: '20px', margin: '15px 0' }}>
-            <img src={agendaIcon} style={{ width: '50px', height: '50px', marginRight: '10px' }} />
-            Tarefas disponíveis
-          </button>
-
-          <button className='botao' style={{ display: 'flex', alignItems: 'center', padding: '20px', fontSize: '20px', margin: '15px 0' }}>
-            <img src={pedidoIcon} style={{ width: '50px', height: '50px', marginRight: '10px' }} />
-            Criar nova tarefa
-          </button>
       </div>
     </main>
   );
